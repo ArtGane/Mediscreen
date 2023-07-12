ea24b23f9b150f4a9d9de52d2191fa49265e2ab9
ArtGane <argane.samson@gmail.com>
2023-07-12 18:55:09 +0200

    first commit after spring initializer

---
+++ diabete/src/main/java/com/mediscreen/diabete/DiabeteDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteDetectorApplication.class, args);
+	}
+
+}
---
+++ diabete/src/test/java/com/mediscreen/diabete/DiabeteDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeteDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
