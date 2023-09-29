8fc996dd606f6900cd6372ec2a468c321994861a
ArtGane <argane.samson@gmail.com>
2023-09-29 07:59:14 +0200

    ajout des risques et de leur niveau

---
+++ assessment-conf/src/main/java/com/mediscreen/assessment/dto/PatientDto.java
@@ -0,0 +1,90 @@
+package com.mediscreen.assessment.dto;
+
+import java.time.LocalDate;
+
+public class PatientDto {
+
+    private Long id;
+    private String family;
+    private String given;
+    private LocalDate dob;
+    private String sex;
+    private String address;
+    private String phone;
+    private int age;
+
+    public PatientDto() { }
+
+    public PatientDto(Long id, String family, String given, LocalDate dob, String sex) {
+        this.id = id;
+        this.family = family;
+        this.given = given;
+        this.dob = dob;
+        this.sex = sex;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getGiven() {
+        return given;
+    }
+
+    public void setGiven(String given) {
+        this.given = given;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
